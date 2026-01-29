15d963f329da0ce5d717d2db96a6669417f52be4
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-01-29 17:11:36 +0100

    add simple-planner to tracing.sh

--- utils/tracing/tracing.sh
+++ utils/tracing/tracing.sh
@@ -16,6 +16,7 @@ CONTAINER_SUFFIXES=(
     "perception.point-cloud-object-detection.fused-1"
     "understanding.object-fusion-1"
     "understanding.lanelet2-object-list-prediction-1"
+    "planning.simple-planner-1"
     "planning.trajectory-optimization-1"
     "ackermann-trajectory-control-1"
 )
